a995bd6de4f347882b5bbffbeab428d6f55d6366
Elbrus5642 <satellitprof@gmail.com>
2022-12-26 15:17:40 +0300

    Homework_9 Tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+int totalsum = 0;
+int GetSumNumbers(int m, int n)
+{
+    
+    if (m==n)
+    {
+        return totalsum + n;
+    }
+    else
+    {
+       return totalsum = m + GetSumNumbers(m+1, n);
+    }
+}
+Console.WriteLine(GetSumNumbers(4,8));
